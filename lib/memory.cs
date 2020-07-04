f273c38589e3fb1ae2579513cd3d88e285b947b2
bansheerubber <gary.cactus.email@gmail.com>
2020-07-04 13:11:09 -0700

    memory tester

---
+++ lib/memory.cs
@@ -0,0 +1,23 @@
+function traceMemory(%max) {
+	deleteVariables("$Memory*");
+	
+	$Memory::Count = 0;
+	for(%i = 0; %i < %max; %i++) {
+		if(isObject(%i)) {
+			if($Memory[%i.getClassName()] $= "") {
+				$Memory[$Memory::Count] = %i.getClassName();
+				$Memory::Count++;
+			}
+			
+			$Memory[%i.getClassName()]++;
+		}
+	}
+
+	printMemoryReport();
+}
+
+function printMemoryReport() {
+	for(%i = 0; %i < $Memory::Count; %i++) {
+		talk($Memory[$Memory::Count] SPC $Memory[$Memory[$Memory::Count]]);
+	}
+}
