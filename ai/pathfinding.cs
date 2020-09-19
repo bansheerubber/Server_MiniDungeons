d64edb2773d63c9edc4b02cb3322fb2de1c3edf9
bansheerubber <gary.cactus.email@gmail.com>
2020-09-19 13:24:30 -0700

    improved node finding
    prefer nodes that are closest on the z-axis

--- ai/pathfinding.cs
+++ ai/pathfinding.cs
@@ -1,7 +1,6 @@
 function clearPathfinding() {
 	deleteVariables("$MD::Node*");
 }
-
 datablock fxDTSBrickData(BrickPathfindingNodeData) {
 	brickFile = "base/data/bricks/flats/1x1f.blb";
 	category = "Special";
@@ -10,107 +9,84 @@ datablock fxDTSBrickData(BrickPathfindingNodeData) {
 	iconName = "base/client/ui/brickIcons/1x1F";
 	isInteractable = true;
 };
-
 function BrickPathfindingNodeData::onPlant(%this, %obj) {
 	Parent::onPlant(%this, %obj);
-	
-	if(!%obj.getGroup().pathfindingNodes) {
+	if( ! %obj.getGroup().pathfindingNodes) {
 		%obj.getGroup().pathfindingNodes = new SimSet();
 	}
-
 	%obj.getGroup().pathfindingNodes.add(%obj);
 }
-
 function BrickPathfindingNodeData::onLoadPlant(%this, %obj) {
 	Parent::onLoadPlant(%this, %obj);
-	
-	if(!%obj.getGroup().pathfindingNodes) {
+	if( ! %obj.getGroup().pathfindingNodes) {
 		%obj.getGroup().pathfindingNodes = new SimSet();
 	}
-
 	%obj.getGroup().pathfindingNodes.add(%obj);
 }
-
 function BrickPathfindingNodeData::onInteract(%this, %obj, %interactee) {
 	if(%interactee.client.buildmode) {
 		%obj.isGraphMode = true;
-		if(!isEventPending(%obj.visualizeNeighbors)) {
-			if(!isObject(%obj.debugLines)) {
+		if( ! isEventPending(%obj.visualizeNeighbors)) {
+			if( ! isObject(%obj.debugLines)) {
 				%obj.debugLines = new SimSet();
 			}
-			
 			%obj.visualizeNeighbors();
 		}
-
 		cancel(%obj.stopVisualizeNeighbors);
 		%obj.stopVisualizeNeighbors = %obj.schedule(120000, stopVisualizeNeighbors);
 	}
 }
-
 function BrickPathfindingNodeData::onLook(%this, %obj, %interactee) {
 	if(%interactee.client.buildmode) {
-		if(!isEventPending(%obj.visualizeNeighbors)) {
-			if(!isObject(%obj.debugLines)) {
+		if( ! isEventPending(%obj.visualizeNeighbors)) {
+			if( ! isObject(%obj.debugLines)) {
 				%obj.debugLines = new SimSet();
 			}
-			
 			%obj.visualizeNeighbors();
 		}
-
-		if(!%obj.isGraphMode) {
+		if( ! %obj.isGraphMode) {
 			cancel(%obj.stopVisualizeNeighbors);
 			%obj.stopVisualizeNeighbors = %obj.schedule(200, stopVisualizeNeighbors);
 		}
 	}
 }
-
 function BrickPathfindingNodeData::onAdd(%this, %obj) {
 	Parent::onAdd(%this, %obj);
-
-	if(!%obj.isPlanted) {
+	if( ! %obj.isPlanted) {
 		%obj.debugLines = new SimSet();
 		%obj.visualizeNeighbors();
 	}
 }
-
 function BrickPathfindingNodeData::onRemove(%this, %obj) {
 	if(%obj.debugLines) {
 		%obj.debugLines.deleteAll();
 		%obj.debugLines.delete();
 	}
-	
 	Parent::onRemove(%this, %obj);
 }
-
 function fxDTSBrick::stopVisualizeNeighbors(%this) {
 	%this.debugLines.deleteAll();
 	cancel(%this.visualizeNeighbors);
 	%this.lastPosition = "";
 	%this.isGraphMode = false;
 }
-
 function fxDTSBrick::visualizeNeighbors(%this) {
 	cancel(%this.visualizeNeighbors);
-
 	if(%this.lastPosition !$= %this.getPosition()) {
 		%this.debugLines.deleteAll();
-		
 		initContainerRadiusSearch(%this.getPosition(), 14, $TypeMasks::fxBrickObjectType);
 		while(%col = containerSearchNext()) {
 			%raycast = containerRaycast(%this.getPosition(), %col.getPosition(), $TypeMasks::fxBrickObjectType, %this);
 			if(%col != %this && %col.getDatablock().getName() $= "BrickPathfindingNodeData" && getWord(%raycast, 0) == %col) {
-				%this.debugLines.add(drawDebugLine(%this.getPosition(), %col.getPosition(), 0.1, "1 0 0 1", -1));
+				%this.debugLines.add(drawDebugLine(%this.getPosition(), %col.getPosition(), 0.1, "1 0 0 1",  - 1));
 			}
 		}
 		%this.lastPosition = %this.getPosition();
 	}
-
 	%this.visualizeNeighbors = %this.schedule(33, visualizeNeighbors);
 }
-
 function testNavmesh(%brickGroup, %id) {
 	$MD::PathTCP.send("create_navmesh" SPC %id @ "\n");
-
 	%count = %brickGroup.getCount();
 	for(%i = 0; %i < %count; %i++) {
 		%brick = %brickGroup.getObject(%i);
@@ -118,52 +94,37 @@ function testNavmesh(%brickGroup, %id) {
 			$MD::PathTCP.send("add_navmesh" SPC %id SPC %brick.getPosition() @ "\n");
 		}
 	}
-
 	$MD::PathTCP.send("test_navmesh" SPC %id @ "\n");
 }
-
 function addNeighbor(%id1, %id2) {
-	if(!$MD::NodeNeighbor[%id1, %id2]) {
+	if( ! $MD::NodeNeighbor[%id1, %id2]) {
 		$MD::PathTCP.send("neighbor" SPC %id1 SPC %id2 @ "\n");
 		$MD::NodeNeighbor[%id1, %id2] = true;
 		addNeighbor(%id1, %id2);
 	}
 }
-
 function fxDTSBrick::searchNeighbors(%this) {
 	%count = %this.room.pathfindingBricks.getCount();
 	for(%i = 0; %i < %count; %i++) {
 		%brick = %this.room.pathfindingBricks.getObject(%i);
-
 		if(%brick == %this) {
 			continue;
 		}
-
 		%raycast = containerRaycast(%this.getPosition(), %brick.getPosition(), $TypeMasks::fxBrickObjectType, %this);
-		if(
-			(
-				%brick.getName() $= "_node"
-				|| %brick.getDatablock().getName() $= "BrickPathfindingNodeData"
-			)
-			&& getWord(%raycast, 0) == %brick
-			&& vectorDist(%this.getPosition(), %brick.getPosition()) < 14.5
-		) {
+		if((%brick.getName() $= "_node" || %brick.getDatablock().getName() $= "BrickPathfindingNodeData") && getWord(%raycast, 0) == %brick && vectorDist(%this.getPosition(), %brick.getPosition()) < 14.5) {
 			addNeighbor(%this.nodeId, %brick.nodeId);
 			// drawDebugLine(%this.getPosition(), %brick.getPosition(), 0.2, "1 0 0 1", 15000);
 		}
 	}
 }
-
 function buildNodesFromBricks(%brickGroup, %start) {
 	if(%start == 0) {
 		clearPathfinding();
 		echo("Cleared pathfinding");
 	}
-	
 	if($MD::NodeCount $= "") {
 		$MD::NodeCount = 0;
 	}
-
 	// do pathfinding bricks
 	%count = %brickGroup.pathfindingNodes.getCount();
 	for(%i = (%start | 0); (%i < %count) && (%i < %start + 100); %i++) {
@@ -174,10 +135,8 @@ function buildNodesFromBricks(%brickGroup, %start) {
 		$MD::PathTCP.send("add" SPC $MD::NodeCount SPC %brick.getPosition() @ "\n");
 		$MD::NodeCount++;
 	}
-
 	schedule(100, 0, buildNodesFromBricks, %brickGroup, %start + 100);
 }
-
 function buildNeighborsFromBricks(%brickGroup) {
 	%count = %brickGroup.pathfindingNodes.getCount();
 	for(%i = 0; %i < %count; %i++) {
@@ -185,65 +144,51 @@ function buildNeighborsFromBricks(%brickGroup) {
 	}
 	$MD::PathTCP.send("simplify\n");
 }
-
 function getClosestNode(%position) {
-	%nodeId = -1;
+	%nodeId =  - 1;
 	%radius = 20;
 	%minDistance = %radius;
-
+	%minZDistance = %radius;
 	%room = getRoomFromPosition(%position);
 	%count = %room.pathfindingBricks.getCount();
 	for(%i = 0; %i < %count; %i++) {
 		%brick = %room.pathfindingBricks.getObject(%i);
-		if(
-			(
-				%brick.getName() $= "_node"
-				|| %brick.getDatablock().getName() $= "BrickPathfindingNodeData"
-			)
-			&& (%distance = vectorDist(%brick.getPosition(), %position)) < %minDistance
-		) {
+		if((%brick.getName() $= "_node" || %brick.getDatablock().getName() $= "BrickPathfindingNodeData") && ((%zDistance = mAbs(vectorSub((getWord(%brick.getPosition(), 2)), (getWord(%position, 2))))) < %minZDistance || (%zDistance <= %minZDistance(%distance = vectorDist(%brick.getPosition(), %position)) < %minDistance))) {
 			%raycast = containerRaycast(%position, %brick.getPosition(), $TypeMasks::fxBrickObjectType);
 			if(getWord(%raycast, 0) == %brick) {
 				%minDistance = %distance;
+				%minZDistance = %zDistance;
 				%nodeId = %brick.nodeId;
 			}
 		}
 	}
 	return %nodeId;
 }
-
 function createPathfindingClient() {
 	%file = new FileObject();
 	%file.openForRead("Add-Ons/Server_MiniDungeons/pathfinding/server.info");
 	%ip = %file.readLine();
 	%port = %file.readLine();
 	%file.close();
-	
 	%tcp = new TCPObject(MiniDungeonsPathfinding);
 	%tcp.connect(%ip @ ":" @ %port);
 	return %tcp;
 }
-
 function initPathfinding() {
-	$MD::PathTCP = createPathfindingClient();
+	$MD::PathCP = createPathfindingClient();
 }
-
 function visualizePathfinding() {
 	$MD::PathTCP.send("visualize\n");
 }
-
 function MiniDungeonsPathfinding::onConnected(%this) {
 	echo("MiniDungeonsPathfinding - TCP connected");
 }
-
 function MiniDungeonsPathfinding::onDisconnect(%this) {
 	echo("MiniDungeonsPathfinding - TCP disconnected");
 }
-
 function MiniDungeonsPathfinding::onConnectFailed(%this) {
 	echo("MiniDungeonsPathfinding - TCP connection failed");
 }
-
 function MiniDungeonsPathfinding::onLine(%this, %data) {
 	%command = getWord(%data, 0);
 	if(%command $= "path") {
@@ -260,21 +205,17 @@ function MiniDungeonsPathfinding::onLine(%this, %data) {
 		warn("Failed to get path for bot" SPC $MD::PathIDTable[%id]);
 	}
 }
-
 function AiPlayer::requestPath(%this, %startNode, %endNode, %started) {
 	$MD::PathIDTable[$MD::PathIDTableCount | 0] = %this;
 	$MD::PathTCP.send("path" SPC ($MD::PathIDTableCount | 0) SPC %startNode SPC %endNode @ "\n");
 	$MD::PathIDTableCount++;
 }
-
 function AiPlayer::hasPath(%this) {
 	return %this.pathCount != 0;
 }
-
 function AiPlayer::hasPathOrTimeout(%this) {
 	return %this.pathCount != 0 || (getSimTime() > %this.lastPathRequest + 2000);
 }
-
 function AiPlayer::setPath(%this, %path) {
 	%count = getWordCount(%path);
 	%this.pathCount = 0;
@@ -285,7 +226,6 @@ function AiPlayer::setPath(%this, %path) {
 	}
 	%this.currentPathIndex = 0;
 }
-
 function AiPlayer::resetPath(%this) {
 	%this.path = "";
 	%this.pathCount = 0;
